41b3c443117ae4b8a456f62ec559c575794d8793
Artem <artiebears@mail.ru>
2025-01-09 14:22:22 +0300

    fix gh-pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, {useEffect} from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {HashRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
-import { AlertMessage } from './components/Modals/AlertMessage/AlertMessage.jsx';
-import { ChatListPage } from './pages/ChatList/ChatListPage.jsx';
-import { PageChat } from './pages/PageChat/PageChat.jsx';
-import { SelfProfilePage } from './pages/SelfProfilePage/SelfProfilePage.jsx';
+import {AlertMessage} from './components/Modals/AlertMessage/AlertMessage.jsx';
+import {ChatListPage} from './pages/ChatList/ChatListPage.jsx';
+import {PageChat} from './pages/PageChat/PageChat.jsx';
+import {SelfProfilePage} from './pages/SelfProfilePage/SelfProfilePage.jsx';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute.jsx';
 import {fetchCurrentUser} from "./store/userSlice.js";
 import {UserProfilePage} from "./pages/UserProfilePage/UserProfilePage.jsx";
+
 function App() {
     const theme = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', theme);
@@ -24,46 +25,46 @@ function App() {
     }, [dispatch]);
 
     return (
-                <Router>
-                    <AlertMessage />
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                <PrivateRoute>
-                                    <ChatListPage />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/chat/:chatId"
-                            element={
-                                <PrivateRoute>
-                                    <PageChat />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/user/:userId"
-                            element={
-                            <PrivateRoute>
-                                <UserProfilePage />
-                            </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/profile"
-                            element={
-                                <PrivateRoute>
-                                    <SelfProfilePage />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="*" element={<Navigate to="/" replace />} />
-                    </Routes>
-                </Router>
+        <Router>
+            <AlertMessage/>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <ChatListPage/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/chat/:chatId"
+                    element={
+                        <PrivateRoute>
+                            <PageChat/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/user/:userId"
+                    element={
+                        <PrivateRoute>
+                            <UserProfilePage/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <PrivateRoute>
+                            <SelfProfilePage/>
+                        </PrivateRoute>
+                    }
+                />
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="*" element={<Navigate to="/" replace/>}/>
+            </Routes>
+        </Router>
     );
 }
 
